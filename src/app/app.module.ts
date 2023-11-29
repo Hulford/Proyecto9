b66ec9b2e8dd0c2de1e3a12c89dc4ea70b268373
Hulford <moraleshulford@gmail.com>
2023-11-29 05:03:03 -0500

    Informacion de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule} from '@angular/common/http'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
 AppRoutingModule,
+HttpClientModule
 
   ],
   providers: [],
